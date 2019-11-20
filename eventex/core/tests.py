4e524280be0a29c3b4bbbd7310d4df4644eb5e58
Raphael Castro <rafaphcastro@gmail.com>
2019-11-20 17:07:18 -0300

    Home test added

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTestCase(TestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get('/')
+
+    def test_get(self) -> None:
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self) -> None:
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
