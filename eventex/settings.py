f073355a4f19d22a99e89cd7f797fb0e07f9dcc9
Raphael Castro <rafaphcastro@gmail.com>
2019-11-23 14:21:18 -0300

    Subscription post and send email added

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,7 +17,6 @@ from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eventex.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,7 +78,6 @@ default_dburl = f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,11 +97,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -114,9 +110,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Email configuration
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,9 +1,45 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            context = form.cleaned_data
+            body = render_to_string('subscriptions/subscription_email.txt',
+                                    context)
+            recipient_list = [
+                'contato@eventex.com.br',
+                form.cleaned_data['email']
+            ]
+            mail.send_mail(
+                subject='Confirmação de inscrição',
+                message=body,
+                from_email='contato@eventex.com.br',
+                recipient_list=recipient_list
+            )
+            messages.success(request, 'Inscrição realizada com sucesso!')
+            return HttpResponseRedirect('/inscricao/')
+        context = {
+            'form': form
+        }
+        return render(
+            request,
+            'subscriptions/subscription_form.html',
+            context
+        )
+
     context = {
         'form': SubscriptionForm()
     }
-    return render(request, 'subscriptions/subscription_form.html', context)
+    return render(
+        request,
+        'subscriptions/subscription_form.html',
+        context
+    )
