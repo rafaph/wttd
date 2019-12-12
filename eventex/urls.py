3834eff9375bf5de48314db0388c784b213dac12
Raphael Castro <rafaphcastro@gmail.com>
2019-12-11 23:31:21 -0300

    Talks added

--- eventex/urls.py
+++ eventex/urls.py
@@ -6,6 +6,7 @@ import eventex.core.views
 urlpatterns = [
     path('', eventex.core.views.home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
+    path('palestras/', eventex.core.views.talk_list, name='talk_list'),
     path('palestrantes/<slug:slug>/', eventex.core.views.speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
